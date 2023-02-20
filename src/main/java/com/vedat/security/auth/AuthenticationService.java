62ff1a70b7781405caec9310436fe3980e3bbafb
vdtacarr <acarvedat5@gmail.com>
2023-02-20 23:30:09 +0300

    first commit

---
+++ src/main/java/com/vedat/security/auth/AuthenticationService.java
@@ -0,0 +1,46 @@
+package com.vedat.security.auth;
+
+import com.vedat.security.config.JwtService;
+import com.vedat.security.user.User;
+import com.vedat.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+   private final UserRepository userRepository;
+   private final PasswordEncoder passwordEncoder;
+   private final JwtService jwtService;
+
+   private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) {
+          var user = User.builder()
+                  .firstName(request.getFirstName())
+                  .lastName(request.getLastName())
+                  .email(request.getEmail())
+                  .password(passwordEncoder.encode(request.getPassword()))
+                  .build();
+          userRepository.save(user);
+          var token = jwtService.generateToken(user);
+          return AuthenticationResponse.builder().token(token).build();
+    }
+}
